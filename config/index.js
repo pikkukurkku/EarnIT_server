aa9e03f55685135031addf3011711b0fa1f8df70
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-29 12:56:29 +0100

    signup set up complete

---
+++ config/index.js
@@ -0,0 +1,22 @@
+// We reuse this import in order to have access to the `body` property in requests
+const express = require("express");
+
+const logger = require("morgan");
+
+const cookieParser = require("cookie-parser");
+
+const cors = require("cors");
+module.exports = (app) => {
+  app.set("trust proxy", 1);
+
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+    })
+  );
+
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+};
---
+++ db/index.js
@@ -0,0 +1,15 @@
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/final-project-server";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
