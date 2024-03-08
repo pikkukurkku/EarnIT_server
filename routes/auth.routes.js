ddb3546c7d61e7698f8106fba25c19b78ce2dbd5
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-03-08 22:14:25 +0100

    implemented delete user functionality

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
 const QuizInput = require("../models/QuizInput.model");
+const mongoose = require('mongoose');
 
 
 const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
@@ -11,7 +12,6 @@ const router = express.Router();
 const saltRounds = 10;
 
 
-// POST /auth/signup  - Creates a new user in the database
 router.post('/signup/:quizinputId', (req, res, next) => {
   const { name, email, password} = req.body;
   const { quizinputId } = req.params;
@@ -56,43 +56,34 @@ router.post('/signup/:quizinputId', (req, res, next) => {
     });
 });
 
-// POST  /auth/login - Verifies email and password and returns a JWT
+
 router.post('/login', (req, res, next) => {
   const { email, password } = req.body;
-  // Check if email or password are provided as empty string 
   if (email === '' || password === '') {
     res.status(400).json({ message: "Provide email and password." });
     return;
   }
 
-  // Check the users collection if a user with the same email exists
   User.findOne({ email })
     .then((foundUser) => {
     
       if (!foundUser) {
-        // If the user is not found, send an error response
         res.status(401).json({ message: "User not found." })
         return;
       }
 
-      // Compare the provided password with the one saved in the database
       const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
 
       if (passwordCorrect) {
-        // Deconstruct the user object to omit the password
         const { _id, email, name } = foundUser;
-        
-        // Create an object that will be set as the token payload
         const payload = { _id, email, name };
 
-        // Create and sign the token
         const authToken = jwt.sign( 
           payload,
           process.env.TOKEN_SECRET,
           { algorithm: 'HS256', expiresIn: "6h" }
         );
 
-        // Send the token as the response
         res.status(200).json({ authToken: authToken });
       }
       else {
@@ -104,17 +95,25 @@ router.post('/login', (req, res, next) => {
 });
 
 
-// GET  /auth/verify  -  Used to verify JWT stored on the client
 router.get('/verify', isAuthenticated, (req, res, next) => {
-
-  // If JWT token is valid the payload gets decoded by the
-  // isAuthenticated middleware and made available on `req.payload`
   console.log(`req.payload`, req.payload);
-
-  // Send back the object with user data
-  // previously set as the token payload
   res.status(200).json(req.payload);
 });
 
+
+router.delete('/user', isAuthenticated, (req, res, next) => {
+  const userId = req.payload._id; 
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  User.findByIdAndDelete(userId)
+    .then(() =>
+      res.json({ message: `User with ${userId} is removed successfully.` })
+    )
+    .catch((error) => res.json(error));
+});
 
 module.exports = router;
